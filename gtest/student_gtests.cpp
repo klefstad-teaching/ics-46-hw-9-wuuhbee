0c889546c91ca2c9fb45b5bcafc5d9b1e1ddbc0d
wuuhbee <melyn.lim3@gmail.com>
2025-03-14 21:51:42 +0000

    fixed edit_distance within to not run infinitely

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -3,6 +3,8 @@
 #include "dijkstras.h"
 #include "ladder.h"
 
-TEST(Replace, Me) {
-  EXPECT_TRUE(false);
+TEST(Ladder, EditDistanceWithin) {
+  EXPECT_TRUE(edit_distance_within("plane", "pane", 1));
+  EXPECT_TRUE(edit_distance_within("chat", "cheat", 1));
+  EXPECT_FALSE(edit_distance_within("test", "tents", 1));
 }
--- src/ladder.cpp
+++ src/ladder.cpp
@@ -13,12 +13,19 @@ bool edit_distance_within(const std::string& str1, const std::string& str2, int 
     int diffChar = 0;
     int index1 = 0, index2 = 0;
     while (index1 < len1 && index2 < len2){
-        if (str1[index1] != str2[index2])
+        if (str1[index1] != str2[index2]){
             ++diffChar;
+            if (len1 < len2)
+                ++index1;
+            else if (len2 < len1)
+                ++index2;
+            else{
+                ++index1;
+                ++index2;
+            }
+        }
         if (diffChar > 1)
             return false;
-        ++index1;
-        ++index2;
     }
 
     if (index1 < len1 || index2 < len2)
