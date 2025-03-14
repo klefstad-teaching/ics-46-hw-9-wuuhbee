ae2d14d73a0115e2d8fb3a37bc9e1eadaa5103a5
wuuhbee <melyn.lim3@gmail.com>
2025-03-14 20:19:50 +0000

    implemented print path and tested in main

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -1,3 +1,14 @@
+#include "dijkstras.h"
+
 int main () {
-    return 0;
+    Graph G;
+    file_to_graph("src/medium.txt", G);
+    // int destination = 0;
+    vector<int> previous;
+    vector<int> shortestPath = dijkstra_shortest_path(G, 0, previous);
+    
+    for (int i = 0; i < G.numVertices; i++) {
+        vector<int> path = extract_shortest_path(shortestPath, previous, i);
+        print_path(path, shortestPath[i]);
+    }
 }
