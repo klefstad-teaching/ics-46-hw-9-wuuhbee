8eaf7fc8d5fc3f15a114e17c7e318afcd6d7c958
wuuhbee <melyn.lim3@gmail.com>
2025-03-14 22:48:15 +0000

    changed how begin and end word were compared

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -43,7 +43,7 @@ bool is_adjacent(const string& word1, const string& word2) {
 }
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
-    if (begin_word.compare(end_word) == 0) return {begin_word};
+    if (begin_word == end_word) return {begin_word};
     queue<vector<string>> ladder_queue;
     ladder_queue.push({begin_word});
     set<string> visited;
@@ -97,7 +97,7 @@ void verify_word_ladder() {
     // my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
     // my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
     // my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
-    my_assert(generate_word_ladder("work", "work", word_list).size() == 1);
+    my_assert(generate_word_ladder("were", "were", word_list).size() == 1);
     // my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
     // my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
 }
